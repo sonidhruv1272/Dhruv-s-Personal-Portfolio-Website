cbb7c02519a09cbec667763f0e078f3d1d47333a
Dhruv Soni <sonidhruv1272@gmail.com>
2026-08-28 19:48:02 -0400

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,55 +1,93 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/HARSHSHUKLA_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
+    <Container fluid className="resume-section">
+      <Particle />
+
+      <Container>
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
+          <Col
+            md={8}
+            className="resume-about"
+            style={{
+              paddingTop: "50px",
+              paddingBottom: "50px",
+            }}
           >
-            <AiOutlineDownload />
-            &nbsp;Download Resume
-          </Button>
+            <h1 className="project-heading">
+              My <strong className="purple">Resume</strong>
+            </h1>
+
+            <p
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontSize: "1.1em",
+                padding: "20px",
+              }}
+            >
+              You can view or download my full resume below, which includes my
+              education, technical experience, engineering projects, and
+              skills in Computer Engineering, embedded systems, robotics,
+              hardware, and software development.
+            </p>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                paddingBottom: "30px",
+              }}
+            >
+              <Button
+                variant="primary"
+                href="/resume.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineDownload />
+                &nbsp; View / Download Resume
+              </Button>
+            </div>
+          </Col>
         </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download Resume
-          </Button>
+        <Row
+          style={{
+            justifyContent: "center",
+            position: "relative",
+            paddingBottom: "50px",
+          }}
+        >
+          <Col md={10}>
+            <div
+              style={{
+                width: "100%",
+                height: "1000px",
+                overflow: "hidden",
+              }}
+            >
+              <iframe
+                src="/resume.pdf"
+                title="Dhruv Soni Resume"
+                width="100%"
+                height="100%"
+                style={{
+                  border: "none",
+                  borderRadius: "8px",
+                  backgroundColor: "white",
+                }}
+              />
+            </div>
+          </Col>
         </Row>
       </Container>
-    </div>
+    </Container>
   );
 }
 
