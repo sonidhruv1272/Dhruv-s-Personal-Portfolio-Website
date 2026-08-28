11cd98b9a29e8c97a86ad0b157b7987ee804c7fa
Dhruv Soni <sonidhruv1272@gmail.com>
2026-08-28 19:44:46 -0400

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,45 +3,72 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/image.png";
-
 
 function About() {
   return (
     <Container fluid className="about-section">
       <Particle />
+
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col
-            md={7}
+            md={8}
             style={{
-              justifyContent: "center",
               paddingTop: "30px",
               paddingBottom: "50px",
             }}
           >
             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Get to Know  <strong className="purple">ME</strong>
+              GET TO KNOW <strong className="purple">ME</strong>
             </h1>
+
             <Aboutcard />
           </Col>
+
           <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            md={4}
+            style={{
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img
+              src={require("../../Assets/about.png")}
+              alt="Computer Engineering"
+              className="img-fluid"
+            />
           </Col>
         </Row>
+
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          Professional <strong className="purple">Skillset</strong>
         </h1>
 
         <Techstack />
 
-        
+        <Row style={{ justifyContent: "center", paddingTop: "50px" }}>
+          <Col md={12}>
+            <h1 className="project-heading">
+              <strong className="purple">Engineering</strong> Interests
+            </h1>
 
-        
+            <p
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontSize: "1.1em",
+                padding: "20px",
+              }}
+            >
+              I am particularly interested in building systems where hardware
+              and software work together. My main areas of interest include
+              embedded systems, firmware development, robotics, IoT, PCB
+              design, sensor integration, real-time control, and hardware-software
+              integration.
+            </p>
+          </Col>
+        </Row>
       </Container>
     </Container>
   );
