bbed524cea7e3fc0cfa0b0d9236ba99711539ce6
Dhruv Soni <sonidhruv1272@gmail.com>
2026-08-28 19:43:26 -0400

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/head-shot.jpg";
+import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -10,19 +10,17 @@ function Home() {
     <section>
       <Container fluid className="home-section" id="home">
         <Particle />
+
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
+                Hi There! <span className="wave">👋🏻</span>
               </h1>
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> HARSH SHUKLA</strong>
+                <strong className="main-name"> DHRUV SONI</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -33,14 +31,15 @@ function Home() {
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
                 src={homeLogo}
-                alt="home pic"
-                className="head-shot"
+                alt="Computer Engineering"
+                className="img-fluid"
                 style={{ maxHeight: "450px" }}
               />
             </Col>
           </Row>
         </Container>
       </Container>
+
       <Home2 />
     </section>
   );
