2015a9595f157b4cd81d95f8b322a01942844f5e
Dhruv Soni <sonidhruv1272@gmail.com>
2026-08-28 19:45:32 -0400

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,54 +1,109 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
   DiPython,
-  DiGit,
   DiJava,
+  DiJavascript1,
+  DiGit,
+  DiGithubBadge,
 } from "react-icons/di";
 import {
-  SiFirebase,
-  SiPostgresql,
+  SiCplusplus,
+  SiArduino,
+  SiEspressif,
+  SiMatlab,
+  SiPandas,
+  SiNumpy,
+  SiFlask,
+  SiFastapi,
 } from "react-icons/si";
+import { TbCircuitBattery } from "react-icons/tb";
+import { FaMicrochip } from "react-icons/fa";
 
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+    <div className="tech-icons">
+
+      <div className="tech-icon-item">
+        <SiCplusplus />
+        <p>C++</p>
+      </div>
+
+      <div className="tech-icon-item">
         <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
+        <p>Python</p>
+      </div>
+
+      <div className="tech-icon-item">
         <DiJava />
-      </Col>
-    </Row>
+        <p>Java</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiMatlab />
+        <p>MATLAB</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <DiJavascript1 />
+        <p>JavaScript</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiArduino />
+        <p>Arduino</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiEspressif />
+        <p>ESP32</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <FaMicrochip />
+        <p>Microcontrollers</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <TbCircuitBattery />
+        <p>Circuit Design</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <TbCircuitBattery />
+        <p>PCB Design</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiNumpy />
+        <p>NumPy</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiPandas />
+        <p>pandas</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiFlask />
+        <p>Flask</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <SiFastapi />
+        <p>FastAPI</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <DiGit />
+        <p>Git</p>
+      </div>
+
+      <div className="tech-icon-item">
+        <DiGithubBadge />
+        <p>GitHub</p>
+      </div>
+
+    </div>
   );
 }
 
