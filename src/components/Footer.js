078353e04e77028a16a824890d87e40d6a29213b
Dhruv Soni <sonidhruv1272@gmail.com>
2026-09-05 15:35:04 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,27 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiFillLinkedin,
-  AiOutlineMail,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function Footer() {
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Dhruv Soni</h3>
+          <h3>Designed and Developed by Dhruv Soni</h3>
         </Col>
 
         <Col md="4" className="footer-copywright">
-          <h3>
-            Designed and Developed by Dhruv Soni
-          </h3>
+          <h3>Copyright © 2026 DS</h3>
         </Col>
 
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/dhruv-soni-6a7a12334/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="LinkedIn"
-              >
-                <AiFillLinkedin />
-              </a>
-            </li>
-
-            <li className="social-icons">
-              <a
                 href="https://github.com/sonidhruv1272"
                 style={{ color: "white" }}
                 target="_blank"
-                rel="noopener noreferrer"
-                aria-label="GitHub"
+                rel="noreferrer"
               >
                 <AiFillGithub />
               </a>
@@ -48,26 +29,15 @@ function Footer() {
 
             <li className="social-icons">
               <a
-                href="mailto:sonidhruv1272@gmail.com"
+                href="https://www.linkedin.com/in/dhruv-soni-6a7a12334/"
                 style={{ color: "white" }}
-                aria-label="Email"
+                target="_blank"
+                rel="noreferrer"
               >
-                <AiOutlineMail />
+                <AiFillLinkedin />
               </a>
             </li>
           </ul>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col
-          md="12"
-          className="footer-copywright"
-          style={{ textAlign: "center", paddingTop: "10px" }}
-        >
-          <p>
-            Copyright © {new Date().getFullYear()} DS
-          </p>
         </Col>
       </Row>
     </Container>
